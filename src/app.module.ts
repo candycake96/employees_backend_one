d6d32b75869aa62a14061698c63b67220caf9464
candycake <candycake96@gmail.com>
2025-12-18 15:02:58 +0700

    creact database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserinfoModule } from './userinfo/userinfo.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UserinfoModule],
+  imports: [UserinfoModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
